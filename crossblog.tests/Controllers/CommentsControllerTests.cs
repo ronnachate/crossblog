3aec59bcaef5657ec749c95da0992f8fb613c86a
Ronnachate <ronnachate@gmail.com>
2018-05-27 23:04:40 +0700

    Add test for comment controllers.

--- crossblog.tests/Controllers/CommentsControllerTests.cs
+++ crossblog.tests/Controllers/CommentsControllerTests.cs
@@ -50,5 +50,85 @@ namespace crossblog.tests.Controllers
 
             Assert.Equal(3, content.Comments.Count());
         }
+
+        [Fact]
+        public async Task Get_ArticleNotFound()
+        {
+            // Act
+            var result = await _commentsController.Get(2);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as NotFoundResult;
+            Assert.NotNull(objectResult);
+        }
+
+        [Fact]
+        public async Task GetCommentById()
+        {
+            // Arrange
+            _articleRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Article>(Builder<Article>.CreateNew().Build()));
+            _commentRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Comment>(Builder<Comment>.CreateNew().With(c => c.ArticleId = 1).Build()));
+
+            // Act
+            var result = await _commentsController.Get(1, 1);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as OkObjectResult;
+            Assert.NotNull(objectResult);
+
+            var comment = objectResult.Value as CommentModel;
+            Assert.NotNull(comment);
+
+            Assert.Equal("Title1", comment.Title);
+        }
+
+        [Fact]
+        public async Task GetCommentWithWrongArticleId()
+        {
+            // Arrange
+            _articleRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Article>(Builder<Article>.CreateNew().Build()));
+            _commentRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Comment>(Builder<Comment>.CreateNew().With(c => c.ArticleId = 1).Build()));
+
+            // Act
+            var result = await _commentsController.Get(2, 1);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as NotFoundResult;
+            Assert.NotNull(objectResult);
+        }
+
+        [Fact]
+        public async Task CreateWithValidData()
+        {
+            // Arrange
+            _articleRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Article>(Builder<Article>.CreateNew().Build()));
+            var commentModel = new CommentModel
+            {
+                Email = "new email",
+                Title = "new title",
+                Content = "new content",
+                Date = DateTime.UtcNow,
+                Published = true
+            };
+            // Act
+            var result = await _commentsController.Post(1, commentModel);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as CreatedResult;
+            Assert.NotNull(objectResult);
+            var content = objectResult.Value as CommentModel;
+            Assert.NotNull(content);
+
+            Assert.Equal("new title", content.Title);
+        }
+
     }
 }
