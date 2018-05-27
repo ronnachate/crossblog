76ec693201e9f026450424afc8e197f797958c81
Ronnachate <ronnachate@gmail.com>
2018-05-27 21:45:53 +0700

    Init comment controllers test.

---
+++ crossblog.tests/Controllers/CommentsControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using crossblog.Controllers;
+using crossblog.Domain;
+using crossblog.Model;
+using crossblog.Repositories;
+using FizzWare.NBuilder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+using Newtonsoft.Json;
+
+namespace crossblog.tests.Controllers
+{
+    public class CommentsControllerTests
+    {
+        private ArticlesController _articlesController;
+        private CommentsController _commentsController;
+
+        private Mock<IArticleRepository> _articleRepositoryMock = new Mock<IArticleRepository>();
+        private Mock<ICommentRepository> _commentRepositoryMock = new Mock<ICommentRepository>();
+
+        public CommentsControllerTests()
+        {
+            _commentsController = new CommentsController(_articleRepositoryMock.Object, _commentRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task GetCommentsInArticle()
+        {
+            // Arrange
+            _articleRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Article>(Builder<Article>.CreateNew().Build()));
+            var commentDbSetMock = Builder<Comment>.CreateListOfSize(3).All().With(c => c.ArticleId = 1).Build().ToAsyncDbSetMock();
+            _commentRepositoryMock.Setup(m => m.Query()).Returns(commentDbSetMock.Object);
+
+            // Act
+            var result = await _commentsController.Get(1);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as OkObjectResult;
+            Assert.NotNull(objectResult);
+
+            var content = objectResult.Value as CommentListModel;
+            Assert.NotNull(content);
+
+            Assert.Equal(3, content.Comments.Count());
+        }
+    }
+}
