5f1428a80dc441940b7ec2f2a3168c808ccbb934
Ronnachate <ronnachate@gmail.com>
2018-05-27 21:16:26 +0700

    Add more article controllers test.

--- crossblog.tests/Controllers/ArticlesControllerTests.cs
+++ crossblog.tests/Controllers/ArticlesControllerTests.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
+using Newtonsoft.Json;
 
 namespace crossblog.tests.Controllers
 {
@@ -104,6 +105,116 @@ namespace crossblog.tests.Controllers
             Assert.NotNull(content);
 
             Assert.Equal("Title1", content.Title);
+        }
+
+        [Fact]
+        public async Task CreateWithValidData()
+        {
+            var artitleModel = new ArticleModel
+            {
+                Title = "new title",
+                Content = "new content",
+                Date = DateTime.Now,
+                Published = false
+            };
+            // Act
+            var result = await _articlesController.Post(artitleModel);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as CreatedResult;
+            Assert.NotNull(objectResult);
+            var content = objectResult.Value as Article;
+            Assert.NotNull(content);
+
+            Assert.Equal("new title", content.Title);
+        }
+
+        [Fact]
+        public async Task CreateWithInvalidData()
+        {
+            var artitleModel = new ArticleModel
+            {
+                Content = "new content",
+                Date = DateTime.Now,
+                Published = false
+            };
+            _articlesController.ModelState.AddModelError("validArticle", "validArticle");
+            // Act
+            var result = await _articlesController.Post(artitleModel);
+            // Assert
+            Assert.NotNull(result);
+            var objectResult = result as BadRequestObjectResult;
+            Assert.NotNull(objectResult);
+        }
+
+        [Fact]
+        public async Task UpdateItemData()
+        {
+            // Arrange
+            _articleRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Article>(Builder<Article>.CreateNew().Build()));
+
+            // Act
+            var result = await _articlesController.Get(1);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as OkObjectResult;
+            Assert.NotNull(objectResult);
+
+            var beforeUpdate = objectResult.Value as ArticleModel;
+            Assert.NotNull(beforeUpdate);
+
+            var artitleModel = new ArticleModel
+            {
+                Title = "new title",
+                Content = "new content"
+            };
+            result = await _articlesController.Put(1, artitleModel);
+            Assert.NotNull(result);
+            //update success
+            objectResult = result as OkObjectResult;
+            Assert.NotNull(objectResult);
+             // Act
+            result = await _articlesController.Get(1);
+
+            // Assert
+            Assert.NotNull(result);
+
+            objectResult = result as OkObjectResult;
+            Assert.NotNull(objectResult);
+
+            var afterUpdate = objectResult.Value as ArticleModel;
+            Assert.NotNull(afterUpdate);
+            Assert.Equal( artitleModel.Title, afterUpdate.Title);
+            Assert.Equal( artitleModel.Content, afterUpdate.Content);
+        }
+
+        [Fact]
+        public async Task Delete()
+        {
+            // Arrange
+            _articleRepositoryMock.Setup(m => m.GetAsync(1)).Returns(Task.FromResult<Article>(Builder<Article>.CreateNew().Build()));
+
+            // Act
+            var result = await _articlesController.Get(1);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var objectResult = result as OkObjectResult;
+            Assert.NotNull(objectResult);
+
+            var obj = objectResult.Value as ArticleModel;
+            Assert.NotNull(obj);
+            result = await _articlesController.Delete(1);
+            // Assert
+            Assert.NotNull(result);
+            var deleteResult = (OkResult)result;
+             Assert.NotNull(deleteResult);
+
         }
     }
 }
